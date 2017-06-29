a714d7efdf7eb1e73f58fa51ccab7b1d382c310b
Jared Andrews <jared.andrews07@gmail.com>
2017-06-29 17:18:07 -0500

    move example matrix to tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,12 +2,12 @@ from pytfmpval import tfmp
 
 
 def test_score():
-    m = tfmp.create_matrix("MA0045.pfm")
+    m = tfmp.create_matrix("tests/MA0045.pfm")
 
     assert round(tfmp.pval2score(m, 0.00001), 2) == 8.77
 
 
 def test_pval():
-    m = tfmp.create_matrix("MA0045.pfm")
+    m = tfmp.create_matrix("tests/MA0045.pfm")
 
     assert round(tfmp.score2pval(m, 8.7708), 5) == 0.00001
