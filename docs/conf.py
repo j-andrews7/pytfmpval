d091b013ac33bd36ed146a616ea80ce09ba9d23b
Jared Andrews <jared.andrews07@gmail.com>
2017-06-29 17:12:54 -0500

    update doc

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../pytfmpval'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('./pytfmpval'))
+sys.path.insert(0, os.path.abspath('./'))
 
 # -- General configuration ------------------------------------------------
 
