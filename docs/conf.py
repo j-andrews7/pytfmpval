bd60743d36109d66a8fa24fe7c185a63aa9840fa
Jared Andrews <jared.andrews07@gmail.com>
2017-06-30 10:14:16 -0500

    mock

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,6 @@
 #
 import sys
 import os
-import pytfmpval
 
 from unittest.mock import MagicMock
 
@@ -29,7 +28,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['pytfmpval']
+MOCK_MODULES = ['pytfmpval', 'pytfmpval.pytfmpval']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 sys.path.insert(0, os.path.abspath('../pytfmpval'))
